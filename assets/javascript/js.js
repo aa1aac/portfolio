03fa02093c252133fa4e96be54f12893933e7dfa
aa1aac <ach.aayush.111@gmail.com>
2018-06-06 16:49:36 +0545

    function

---
+++ assets/javascript/js.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    var window = $(window);
+var elem = $(".num")
+
+function isScrolledIntoView(elem, window) {
+    var docViewTop = window.scrollTop();
+    var docViewBottom = docViewTop + window.height();
+
+    var elemTop = elem.offset().top;
+    var elemBottom = elemTop + elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+$(document).on("scroll", function () {
+    if (isScrolledIntoView(elem, window)) {
+        elem.addClass("animate")
+    }
+});
+$('.carousel').carousel({
+    interval: 1000
+  })
+})
